63ed7a614af2c4302e04ecb0976d4754a7847784
deepakbalor2493@gmail.com <deepakbalor2493@gmail.com>
2025-02-05 20:04:40 +0530

    changes made

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def generate_response(prompt):
     return res.text
 
 # Streamlit App UI
-st.title("TRx ✨")
+st.title(" 𝕋ℝ𝕩 𝔹𝕆𝕋 ✨")
 
 # Initialize chat history
 if "messages" not in st.session_state:
